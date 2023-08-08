75bd80089416210e3c46e57ecbe6dc2e245c5375
kanayofirms <kanyeze@gmail.com>
2023-08-08 14:35:50 +0100

    Create routes to delete book

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -10,5 +10,6 @@ var RegisterBookStore = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 	
 }
