b4ee287e47f19319cfa1815727e5cccc72311876
kanayofirms <kanyeze@gmail.com>
2023-08-10 20:08:39 +0100

    Refactor routes for functionality

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -6,7 +6,7 @@ import (
 )
 	
 
-var RegisterBookStore = func(router *mux.Router) {
+var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
